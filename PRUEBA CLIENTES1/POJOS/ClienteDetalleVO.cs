95fd7340de50d5f0b7c85d5599cf939944667a20
VictorG <victorgarciaa93@gmail.com>
2019-03-01 11:19:07 -0600

    Se agregó la clase ClientesDetalladoVO

---
+++ PRUEBA CLIENTES1/POJOS/ClienteDetalleVO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRUEBA_CLIENTES1.POJOS
+{
+
+    class ClienteDetalleVO
+    {
+        private string id {get;set;}
+        private string nombre{get;set;}
+        private float cargos{get;set;}
+        private float abono30 {get;set;}
+        private float abono60 {get;set;}
+        private float abono90 {get;set;}
+        private float abonoPlus90 {get;set;}
+    
+    }
+    
+
+}
+}
